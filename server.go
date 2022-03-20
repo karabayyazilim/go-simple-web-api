67705eba4935cd8587049f86112cda7ccdb89545
Ali Karabay <alikarabay77@gmail.com>
2022-03-20 20:30:57 +0300

    initial commit

---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/karabayyazilim/golang-gin-web-api/controller"
+	"github.com/karabayyazilim/golang-gin-web-api/service"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
+	})
+
+	server.Run(":8080")
+}
